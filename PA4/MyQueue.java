07046688d2494f6671fa2c5b4fc2d66cb3402a59
Muchan Li <limuchan28@gmail.com>
2023-10-23 22:48:04 -0700

    PA4 startercode

---
+++ PA4/MyQueue.java
@@ -0,0 +1,55 @@
+/*
+    Name: TODO
+    PID:  TODO
+ */
+
+/**
+ * TODO
+ *
+ * @author TODO
+ * @since TODO
+ */
+
+public class MyQueue<T> implements MyQueueInterface<T> {
+    
+    /* instance variables, feel free to add if you need */
+
+    /* ===separation=== */
+
+    public MyQueue() {
+        // TODO
+    }
+
+    public MyQueue(int capacity) {
+        // TODO
+    }
+
+    public boolean isEmpty() {
+        // TODO
+        return false;
+    }
+
+    public int size() {
+        // TODO
+        return 0;
+    }
+
+    public void clear() {
+        // TODO
+    }
+
+    public void enqueue(T data) {
+        // TODO
+    }
+
+    public T dequeue() {
+        // TODO
+        return null;
+    }
+
+    public T peek() {
+        // TODO
+        return null;
+    }
+
+}
